9a983b4084ec4021007a94f267778ef4b12fb319
franreno <franreno@DESKTOP-59MLBG2.localdomain>
2020-11-26 15:27:54 -0300

    Creation of T3

---
+++ T3/main.c
@@ -0,0 +1,21 @@
+// fazer um que colocar em ordem
+
+
+int main()
+{
+
+    char alf = {"a" , "b" , "c" , "d" ,
+                "e" , "f" , "g" , "h" ,
+                "i" , "j" , "k" , "l" ,
+                "m" , "n" , "o" , "p" ,
+                "q" , "r" , "s" , "t" , 
+                "u" , "v" , "w" , "x" ,
+                "y" , "z"};
+                
+    char str[2][32];
+    
+    scanf("%s %s" , str[0], str[1]);
+
+    // organizar elas em ordem alfabetica.
+
+}
